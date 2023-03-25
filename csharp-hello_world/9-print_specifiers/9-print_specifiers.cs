2e894e8dbc870df7c249a375b658d3420dd97df9
CarlosMatallana-prog <3496@holbertonschool.com>
2023-03-24 19:21:55 -0500

    Format double

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine($"{percent * 100}%");
+		Console.WriteLine($"Currency: {currency:C2}");
+	}
+}

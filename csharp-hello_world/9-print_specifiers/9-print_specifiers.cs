76060d99c97df701749cf54c8c6be622c769c536
CarlosMatallana-prog <3496@holbertonschool.com>
2023-03-24 19:24:13 -0500

    Fix output

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-		Console.WriteLine($"{percent * 100}%");
+		Console.WriteLine($"Percent: {percent * 100}%");
 		Console.WriteLine($"Currency: {currency:C2}");
 	}
 }

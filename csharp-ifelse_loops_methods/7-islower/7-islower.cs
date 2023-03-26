a7d459610d5e3476dcaf515860cb90cc405a6409
CarlosMatallana-prog <3496@holbertonschool.com>
2023-03-26 13:35:03 -0500

    Check upper and lower case

---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        int letter = (int)c;
+        if (letter > 96 && letter < 123)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+

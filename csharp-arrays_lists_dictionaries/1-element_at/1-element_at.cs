558d7332e79365d9080014d568d780817bd7d80e
CarlosMatallana-prog <3496@holbertonschool.com>
2023-03-28 18:58:06 -0500

    Fix output

--- csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
+++ csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -4,11 +4,17 @@ class Array
 {
     public static int elementAt(int[] array, int index)
     {
-        if (index > array.Length || index < 0)
+        Console.WriteLine(array.Length);
+        Console.WriteLine(index);
+
+        if (index >= array.Length || index < 0)
         {
             Console.WriteLine("Index out of range");
             return -1;
         }
+        else
+        {
         return array[index];
+        }
     }
 }

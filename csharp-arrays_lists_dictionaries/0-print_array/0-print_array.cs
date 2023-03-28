1f1f780547fa373e0f8653f10a6c959e80873255
CarlosMatallana-prog <3496@holbertonschool.com>
2023-03-28 18:15:11 -0500

    Fix files names

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+public class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        int[] newArray = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            newArray[i] = i;
+            Console.Write($"{i} ");
+        }
+
+        Console.WriteLine();
+        return newArray;
+    }
+}

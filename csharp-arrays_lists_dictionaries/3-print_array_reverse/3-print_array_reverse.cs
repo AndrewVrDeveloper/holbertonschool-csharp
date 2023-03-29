bc18d9f835f796e1b93845fa1eeb0c378daaa610
CarlosMatallana-prog <3496@holbertonschool.com>
2023-03-29 15:13:45 -0500

    Reverse array

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        var sb = new StringBuilder();
+        if (array != null)
+        {
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                if (i > 0)
+                {
+                    sb.Append(array[i]).Append(" ");
+                }
+                else
+                {
+                    sb.Append(array[i]);
+                }
+            }
+        }
+        Console.WriteLine(sb.ToString());
+    }
+}

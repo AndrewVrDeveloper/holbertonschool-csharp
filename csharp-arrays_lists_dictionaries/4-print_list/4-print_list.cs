ded8894afc51c2d91a931f921f50af76f0fac469
CarlosMatallana-prog <3496@holbertonschool.com>
2023-03-30 11:25:26 -0500

    Create and print list number

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        List<int> numbers = new List<int>(size);
+        int i;
+        for (i = 0; i < size; i++)
+        {
+            if (i == size - 1)
+            {
+                numbers.Add(i);
+                Console.WriteLine($"{i}");
+            }
+            else
+            {
+                numbers.Add(i);
+                Console.Write($"{i} ");
+            }
+        }
+        return numbers;
+    }
+}

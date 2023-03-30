be6e8fff4c0ded5c9392baf28f0c832479ad8a5b
CarlosMatallana-prog <3496@holbertonschool.com>
2023-03-30 15:02:40 -0500

    Delete a position in a list

---
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index >= myList.Count || index < 0)
+        {
+            Console.WriteLine("Index is out of range");
+            return myList;
+        }
+
+        List<int> newList = new List<int>();
+
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (i != index)
+            {
+                newList.Add(myList[i]);
+            }
+        }
+        myList = newList;
+        return myList;
+    }
+}
---
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-main.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> myList = new List<int>() {1, 2, 3, 4, 5};
+
+        foreach (int i in myList)
+            Console.WriteLine(i);
+
+        Console.WriteLine("----------");
+
+
+        List<int> otherList = new List<int>();
+
+        otherList = List.DeleteAt(myList, 2);
+
+        foreach (int i in otherList)
+            Console.WriteLine(i);
+    }
+}

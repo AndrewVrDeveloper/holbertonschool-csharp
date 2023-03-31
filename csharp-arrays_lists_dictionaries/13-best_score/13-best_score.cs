e060062b0b6b7fbbb41cc01415f976efbed0c11c
CarlosMatallana-prog <3496@holbertonschool.com>
2023-03-31 18:05:32 -0500

    Best dictionary key by value

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string maxKey = "None";
+        int maxValue = 0;
+        foreach (KeyValuePair<string, int> entry in myList)
+        {
+            if (entry.Value > maxValue)
+            {
+                maxKey = entry.Key;
+                maxValue = entry.Value;
+            }
+        }
+        return maxKey;
+    }
+}
